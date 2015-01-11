2232dfe305add90e90f40bc8bb85851604cf5aab
Ardeshir Javaherchi <ardeshir.javaherchi@gmail.com>
2015-01-11 14:38:27 -0800

    Add log out and update log in process

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,9 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-
+  
   def destroy
-
+    log_out
+    redirect_to root_url
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -29,6 +29,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
       end
       assert_template 'users/show'
+      assert is_logged_in?
   end
 
 end
