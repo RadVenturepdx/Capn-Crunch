153fba38dcef93c3949d227760f494eb187f3367
Ardeshir Javaherchi <ardeshir.javaherchi@gmail.com>
2015-01-10 23:37:29 -0800

    Add home page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get    'signup' => 'users#new'
+  root                'static_pages#home'
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
