fd5d83b52ed94fc38ea9deac7a4f190e1c192778
Brad Beazley <beazley2@pdx.edu>
2015-01-31 11:09:24 -0800

    Bring in mountain static pages and populate with top three guides

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root                'static_pages#index'
-  get    'contact' => 'static_pages#contact'
-  get    'about'   => 'static_pages#about'
-  get    'faq'     => 'static_pages#faq'
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get    'contact'    => 'static_pages#contact'
+  get    'about'      => 'static_pages#about'
+  get    'faq'        => 'static_pages#faq'
+  get    'mtbachelor' => 'static_pages#mtbachelor'
+  get    'mthood'     => 'static_pages#mthood'
+  get    'mtwhistler' => 'static_pages#mtwhistler'
+  get    'signup'     => 'users#new'
+  get    'login'      => 'sessions#new'
+  post   'login'      => 'sessions#create'
+  delete 'logout'     => 'sessions#destroy'
   resources :users
   resources :guides
   # The priority is based upon order of creation: first created -> highest priority.
