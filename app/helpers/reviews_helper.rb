d2684a0d69c5510b9493599b4b1c6d5835492886
Ardeshir Javaherchi <ardeshir.javaherchi@gmail.com>
2015-01-30 19:47:08 -0800

    Use average method to calculate rating stars

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,10 +1,2 @@
 module ReviewsHelper
-  def overall_rating_stars(reviews)
-    totalRating = 0
-    for review in reviews
-      totalRating += review.rating
-    end
-    avgRating = totalRating / reviews.size
-    ratingStars = avgRating % 5
-  end
 end
