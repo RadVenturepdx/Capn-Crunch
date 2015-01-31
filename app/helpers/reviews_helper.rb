d5763aead29e7da7a5f190fe2d861d9b5a9bbaf1
Ardeshir Javaherchi <ardeshir.javaherchi@gmail.com>
2015-01-30 16:49:25 -0800

    Populate profile information from database

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+  def overall_rating_stars(reviews)
+    totalRating = 0
+    for review in reviews
+      totalRating += review.rating
+    end
+    avgRating = totalRating / reviews.size
+    ratingStars = avgRating % 5
+  end
 end
