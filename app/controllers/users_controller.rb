81a78a1e24e76aeef84d66cfb099ba3176e914cb
sector46 <caseybowman@bpccm.com>
2015-01-31 18:38:02 -0800

    Minor update for flashing
    flash.now is used when a page is rendered. flash is used for a redirect.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
   def logged_in_user
     unless logged_in?
       store_location
-      flash.now[:danger] = "Please log in."
+      flash[:danger] = "Please log in."
       redirect_to login_url
     end
   end
