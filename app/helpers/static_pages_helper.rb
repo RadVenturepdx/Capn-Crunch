5521620b2a7d5cee20fdd8a893e6aff98f13c0c2
Brad Beazley <beazley2@pdx.edu>
2015-02-04 15:13:58 -0800

    Change Mt. static pages to reflect (possible) multiple sports for the top guides

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,7 +3,15 @@ module StaticPagesHelper
     guides = Guide.limit(3)
   end
 
-  def local_guides(location)
-    guides = Guide.where(:location => location)
+  def hood_guides
+    guides = Guide.where(:hood => true)
+  end
+
+  def bachelor_guides
+    guides = Guide.where(:bachelor => true)
+  end
+
+  def whistler_guides
+    guides = Guide.where(:whistler => true)
   end
 end
