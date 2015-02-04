1644386f052f11bbf6a965e7f8a2db74949f5344
Brad Beazley <beazley2@pdx.edu>
2015-02-04 14:59:53 -0800

    Add helper functions for constructing guide location and sports strings

--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -1,2 +1,53 @@
 module GuidesHelper
+  def location_string(hood, bachelor, whistler)
+    comma = false
+    str = ''
+    if hood
+      str << 'Mount Hood'
+      comma = true
+    end
+
+    if bachelor
+      if comma
+        str << ', '
+      end
+      str << 'Mount Bachelor'
+      comma = true
+    end
+
+    if whistler
+      if comma
+        str << ', '
+      end
+      str << 'Whistler'
+    end
+
+    return str
+  end
+
+  def sports_string(downhill, crosscountry, snowboard)
+    comma = false
+    str = ''
+    if downhill
+      str << 'Downhill Skiing'
+      comma = true
+    end
+
+    if crosscountry
+      if comma
+        str << ', '
+      end
+      str << 'Crosscountry Skiing'
+      comma = true
+    end
+
+    if snowboard
+      if comma
+        str << ', '
+      end
+      str << 'Snowboarding'
+    end
+
+    return str
+  end
 end
