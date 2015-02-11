475365831bf1c61a24e536b3be36229dd6d78918
Harmanpreet Singh <singh2@pdx.edu>
2015-02-10 22:54:13 -0800

    updated routes for contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root                                  'static_pages#home'
-  get    'contact'                   => 'static_pages#contact'
+  get    'contact'                   => 'messages#new'
+  get    'contact'                   => 'messages#create'
   get    'locations'                 => 'static_pages#locations'
   get    'about'                     => 'static_pages#about'
   get    'faq'                       => 'static_pages#faq'
@@ -16,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :guides
+  resources :messages
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
