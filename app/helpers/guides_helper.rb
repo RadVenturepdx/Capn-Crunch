6af148f8e0383dd241f84f43b08a89905b25e51b
Brad Beazley <beazley2@pdx.edu>
2015-02-14 14:33:01 -0800

    Make helper functions for top_guide logic, cleaning up the code

--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -63,4 +63,26 @@ module GuidesHelper
 
     return str
   end
+
+  def top_guides(guides)
+    top_guides = []
+    guides.each do |guide|
+      if guide.reviews.count > 0
+        average_rating = guide.reviews.average(:rating)
+        if top_guides.empty? || top_guides[0].reviews.average(:rating) < average_rating
+          if !top_guides[1].nil?
+            top_guides[2] = top_guides[1]
+          end
+          top_guides[1] = top_guides[0]
+          top_guides[0] = guide
+        elsif top_guides[1].nil? || top_guides[1].reviews.average(:rating) < average_rating
+          top_guides[2] = top_guides[1]
+          top_guides[1] = guide
+        elsif top_guides[2].nil? || top_guides[2].reviews.average(:rating) < average_rating
+          top_guides[2] = guide
+        end
+      end
+    end
+    return top_guides
+  end
 end
